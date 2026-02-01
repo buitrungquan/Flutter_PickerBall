bdb2f33f14fbd8ef1a71be5687b04c557ebbcdc3
FLUTTER_MOBILE_PICKEBALL <buiquan02080@gmail.com>
2026-02-01 20:42:10 +0700

    Push latest changes for render debugging

--- MOBILE_FLUTTER_1771020559_buitrungquan/Backend/Program.cs
+++ MOBILE_FLUTTER_1771020559_buitrungquan/Backend/Program.cs
@@ -89,10 +89,7 @@ builder.Services.AddCors(options =>
     {
         policy
             .WithOrigins(
-                        "http://localhost:3000",
-                        "http://localhost:4000",
-                        "http://localhost:5000",
-                        "http://localhost:49691",
+                        "http://localhost",
                         "https://flutter-pickerball.onrender.com"
                     )
             .AllowAnyMethod()
